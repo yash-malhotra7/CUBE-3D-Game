41bc01075cff37f7a488d2108e0992b4f7d2aaed
malhotra-yash <yash.malhotra2020@vitstudent.ac.in>
2022-10-15 22:11:17 +0530

    Final Update

--- MakingAVideoGame/Assets/Scripts/FollowPlayer.cs
+++ MakingAVideoGame/Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
-    public Vector3 offset;
+    //public Vector3 offset;
+    public float smoothening;
+    public float rotationSmoothening;
+
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position = player.transform.position + offset;
+        transform.position = Vector3.Lerp(transform.position, player.position, smoothening);
+        transform.rotation = Quaternion.Slerp(transform.rotation, player.rotation, rotationSmoothening);
+        transform.rotation = Quaternion.Euler(new Vector3(0, transform.rotation.eulerAngles.y, 0));
     }
 }
--- MakingAVideoGame/Assets/Scripts/PlayerMovement.cs
+++ MakingAVideoGame/Assets/Scripts/PlayerMovement.cs
@@ -5,13 +5,13 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public GamePlayUI gamePlayUI;
-
     public Rigidbody rb;
-    public float forwardForce = 2000f;
-    public float sidewaysForce = 500f;
-    public float acceleration = 4f;
+    //public float forwardForce = 2000f;
+    //public float sidewaysForce = 500f;
     public PlayerMovement movement;
+    public float speed;
+    public float turnspeed;
+    public float gravityMultiplier;
 
     private void Start()
     {
@@ -21,28 +21,7 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-
-        /*if(Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Acceleration);
-        }
-        
-        if(Input.GetKey("d"))
-        {
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.Acceleration);
-        }
-
-        if (Input.GetKey("a"))
-        {
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.Acceleration);
-        }
-
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.Acceleration);
-        }*/
-
-        if (Input.GetKey("w"))
+        /*if (Input.GetKey("w"))
         {
             rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Force);
         }
@@ -60,6 +39,29 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey("s"))
         {
             rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.Force);
+        }*/
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * speed);
         }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddRelativeForce(new Vector3(Vector3.forward.x, 0, Vector3.forward.z) * -speed);
+        }
+        Vector3 localVelocity = transform.InverseTransformDirection(rb.velocity);
+        localVelocity.x = 0;
+        rb.velocity = transform.TransformDirection(localVelocity);
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddTorque(Vector3.up * turnspeed);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddTorque(-Vector3.up * turnspeed);
+        }
+
+        rb.AddForce(Vector3.down * gravityMultiplier);
     }
 }
